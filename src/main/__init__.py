0166e08ecd3270b13590bbcd6c5600f3dd9e079e
MonikaP <piechatzek@hotmail.com>
2019-02-17 18:57:22 -0800

    deleted __init__.py

--- src/main/__init__.py
+++
@@ -1,7 +0,0 @@
-from src import feature_extraction as fe
-
-#test
-fe.FeMain.main('../raw_datasets/data_legitimate_1000.txt', False)
-fe.FeMain.main('../raw_datasets/data_legitimate_1000.txt', True)
-
-
