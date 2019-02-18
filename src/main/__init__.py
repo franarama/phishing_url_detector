d94b79b01d7b093506937ac0c4a1edb1e57a0216
MonikaP <piechatzek@hotmail.com>
2019-02-17 18:05:10 -0800

    legit and phish csv and init.py

---
+++ src/main/__init__.py
@@ -0,0 +1,7 @@
+from src import feature_extraction as fe
+
+#test
+fe.FeMain.main('../raw_datasets/data_legitimate_1000.txt', False)
+fe.FeMain.main('../raw_datasets/data_legitimate_1000.txt', True)
+
+
