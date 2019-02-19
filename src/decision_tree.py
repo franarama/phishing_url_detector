66e24004dc9261392ba772fab9be6046fdb59c3e
MonikaP <piechatzek@hotmail.com>
2019-02-19 12:53:22 -0800

    Added basic importing csv files

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+class DecisionTreeMain:
+    def __init__(self, phishing_csv_path, legitimate_csv_path):
+        self.phishing_csv_path = phishing_csv_path
+        self.legitimate_csv_path = legitimate_csv_path
+
+    def main(self):
+        phishing_urls = pd.read_csv(self.phishing_csv_path)
+        legitimate_urls = pd.read_csv(self.legitimate_csv_path)
+
+        
