9c8c81dc640103bba4ba091be1f684333deaaae8
franarama <franramunno@gmail.com>
2019-04-05 15:04:02 -0700

    Add static folder with icon, reformat code

--- src/Routes.py
+++ src/Routes.py
@@ -1,42 +1,45 @@
 from flask import Flask, render_template, request
 from avoidmain import percentage
-url = ''
 
+url = ''
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def urltest():
-	form = request.form
-	if request.method == 'POST':
-		url = request.form['url']
-		print("got the url: " + url)
-		print("the percentage ", percentage(url))
-		percent= percentage(url)
-		return render_template('index.html', percent = percent)
-	return render_template('index.html', form = form)
+    form = request.form
+    if request.method == 'POST':
+        url = request.form['url']
+        print("got the url: " + url)
+        print("the percentage ", percentage(url))
+        percent = percentage(url)
+        return render_template('index.html', percent=percent)
+    return render_template('index.html', form=form)
+
+
 if __name__ == '__main__':
-	app.run()
+    app.run()
 
 
-# CODE OFF PYTHONANYWHERE
-# from flask import Flask, render_template, request
-# from src.avoidmain import percentage
-# url = ''
+    # CODE OFF PYTHONANYWHERE
+    # from flask import Flask, render_template, request
+    # from src.avoidmain import percentage
+    # url = ''
 
 
-# app = Flask(__name__)
+    # app = Flask(__name__)
 
-# @app.route('/', methods = ['POST', 'GET'])
-# def urltest():
-# 	form = request.form
-# 	if request.method == 'POST':
-# 		form = request.form
-# 		url = request.form['url']
-# 		print("got the url: " + url)
-# 		print("the percentage ", percentage(url))
-# 		percent= percentage(url)
-# 		return render_template('index.html', percent = percent)
-# 	return render_template('index.html', form = form)
-# if __name__ == '__main__':
-# 	app.run()
+    # @app.route('/', methods = ['POST', 'GET'])
+    # def urltest():
+    # 	form = request.form
+    # 	if request.method == 'POST':
+    # 		form = request.form
+    # 		url = request.form['url']
+    # 		print("got the url: " + url)
+    # 		print("the percentage ", percentage(url))
+    # 		percent= percentage(url)
+    # 		return render_template('index.html', percent = percent)
+    # 	return render_template('index.html', form = form)
+    # if __name__ == '__main__':
+    # 	app.run()
